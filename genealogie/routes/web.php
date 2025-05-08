800651e1d33a5f0d428dc450b63a4ea9397304b9
Alper Cakirgenc <60131@etu.he2b.be>
2025-05-09 00:50:42 +0200

    added temporary a creat login url juste to have something working

--- genealogie/routes/web.php
+++ genealogie/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PersonController;
 
 Route::get('/', [PersonController::class, 'index'])->name('index');
 Route::get('/show/{id}', [PersonController::class, 'show'])->name('person.show');
+Route::get('/login', [PersonController::class, 'create'])->name('login');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/create', [PersonController::class, 'create'])->name('create');
