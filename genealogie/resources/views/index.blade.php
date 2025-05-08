c07dcb936231062bd29dad699117ea6f2d605054
Alper Cakirgenc <60131@etu.he2b.be>
2025-05-08 22:50:17 +0200

    added a poeple list in the index page

--- genealogie/resources/views/index.blade.php
+++ genealogie/resources/views/index.blade.php
@@ -5,4 +5,20 @@
 @section('content')
     <h1>List of persons (with her creator)</h1>
 
+    <ul class="list-group">
+        @if ($peoples->isNotEmpty())
+            @foreach ($peoples as $person)
+                <li class="list-group-item">
+                    {{ $person->first_name }} {{ $person->last_name }}
+                    <br>
+                    <span class="text-muted">
+                        Create by : {{ $person->creator->name ?? 'User unknown' }}
+                    </span>
+                </li>
+            @endforeach
+        @else
+            <li class="list-group-item">The is no person registered on database.</li>    
+        @endif
+    </ul>
+
 @endsection
